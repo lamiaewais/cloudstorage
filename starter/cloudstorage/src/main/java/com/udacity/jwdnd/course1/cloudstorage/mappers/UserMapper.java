b82f591f049109f3a484c57c9e62c490cda1d5f1
lamiaewais <lamia5980@gmail.com>
2022-11-24 14:24:20 +0300

    design database

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -0,0 +1,21 @@
+package com.udacity.jwdnd.course1.cloudstorage.mappers;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Users;
+import com.udacity.jwdnd.course1.cloudstorage.util.SqlProvider;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.SelectProvider;
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    @SelectProvider(value = SqlProvider.class, method = "selectAllUsers")
+    List<Users> getUsers();
+
+    @InsertProvider(value = SqlProvider.class, method = "insertUser")
+    int insertUser(Users user);
+
+    @SelectProvider(type = SqlProvider.class, method = "getUserByUsername")
+    Users getUserByUsername(String username);
+}
