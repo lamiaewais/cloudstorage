e695eb48c68214d849ac78a0fca08dfbcccaef8b
lamiaewais <lamia5980@gmail.com>
2022-11-27 21:21:51 +0300

    increase size

---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/pages/SignupPage.java
@@ -0,0 +1,49 @@
+package com.udacity.jwdnd.course1.cloudstorage.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+    @FindBy(id = "inputUsername")
+    private WebElement usernameInput;
+
+    @FindBy(id = "inputPassword")
+    private WebElement passwordInput;
+
+    @FindBy(id = "inputFirstName")
+    private WebElement firstnameInput;
+
+    @FindBy(id = "inputLastName")
+    private WebElement lastnameInput;
+
+    @FindBy(id = "buttonSignUp")
+    private WebElement signupButton;
+
+    @FindBy(id = "success-msg")
+    private WebElement successMessage;
+
+    @FindBy(id = "error-msg")
+    private WebElement errorMessage;
+
+    public SignupPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void signup(String username, String password, String firstname, String lastname) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        firstnameInput.sendKeys(firstname);
+        lastnameInput.sendKeys(lastname);
+        signupButton.click();
+    }
+
+    public boolean isSuccessMessageDisplayed() {
+        return successMessage.isDisplayed();
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return errorMessage.isDisplayed();
+    }
+}
